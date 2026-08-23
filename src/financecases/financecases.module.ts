4846698738b4fe3d61c2262ddad2b4de487abb91
mkfinace <KIRANDAHIR@GMAIL.COM>
2026-08-23 15:28:10 +0530

    Create FinanceCasesModule with controller and service

---
+++ src/financecases/financecases.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { FinanceCasesController } from './financecases.controller';
+import { FinanceCasesService } from './financecases.service';
+
+@Module({
+  controllers: [FinanceCasesController],
+  providers: [FinanceCasesService],
+})
+export class FinanceCasesModule {}
