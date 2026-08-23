9089afd30f86fa5cc88858ab0f234e4785df3f95
mkfinace <KIRANDAHIR@GMAIL.COM>
2026-08-23 16:11:56 +0530

    Import AuditLogsModule in LeadsModule

--- src/leads/leads.module.ts
+++ src/leads/leads.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LeadsController } from './leads.controller';
 import { LeadsService } from './leads.service';
+import { AuditLogsModule } from '../auditlogs/auditlogs.module';
 
 @Module({
+  imports: [AuditLogsModule],
   controllers: [LeadsController],
   providers: [LeadsService],
 })
