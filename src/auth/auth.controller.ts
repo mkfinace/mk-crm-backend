324f99e94b32edaf975d59d4859e2c9c2e1ce8e7
mkfinace <KIRANDAHIR@GMAIL.COM>
2026-08-23 10:56:56 +0530

    Refactor OTP request and verification methods

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { RequestOtpDto, VerifyOtpDto } from './auth.dto';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -8,12 +9,12 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('otp/request')
-  requestOtp(@Body('mobile') mobile: string) {
-    return this.authService.requestOtp(mobile);
+  requestOtp(@Body() body: RequestOtpDto) {
+    return this.authService.requestOtp(body.mobile);
   }
 
   @Post('otp/verify')
-  verifyOtp(@Body('mobile') mobile: string, @Body('code') code: string) {
-    return this.authService.verifyOtp(mobile, code);
+  verifyOtp(@Body() body: VerifyOtpDto) {
+    return this.authService.verifyOtp(body.mobile, body.code);
   }
 }
