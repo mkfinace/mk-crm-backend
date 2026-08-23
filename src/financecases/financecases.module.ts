be3a9365f67d4aba71da8d53ba0fbdd0c9a1976d
mkfinace <KIRANDAHIR@GMAIL.COM>
2026-08-23 16:13:46 +0530

    Update financecases.module.ts

--- src/financecases/financecases.module.ts
+++ src/financecases/financecases.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FinanceCasesController } from './financecases.controller';
 import { FinanceCasesService } from './financecases.service';
+import { AuditLogsModule } from '../auditlogs/auditlogs.module';
 
 @Module({
+  imports: [AuditLogsModule],
   controllers: [FinanceCasesController],
   providers: [FinanceCasesService],
 })
