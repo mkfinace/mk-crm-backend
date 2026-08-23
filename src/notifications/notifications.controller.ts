71ee1b0aae8011d251e0341e6a6b4925aa428893
mkfinace <KIRANDAHIR@GMAIL.COM>
2026-08-23 15:49:59 +0530

    Implement NotificationsController with CRUD operations

---
+++ src/notifications/notifications.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { NotificationsService } from './notifications.service';
+import { CreateNotificationDto } from './notifications.dto';
+
+@ApiTags('notifications')
+@Controller('notifications')
+export class NotificationsController {
+  constructor(private notificationsService: NotificationsService) {}
+
+  @Post()
+  createNotification(@Body() data: CreateNotificationDto) {
+    return this.notificationsService.createNotification(data);
+  }
+
+  @Get()
+  listNotifications(@Query('userId') userId: string, @Query('unreadOnly') unreadOnly?: string) {
+    return this.notificationsService.listNotifications(userId, unreadOnly === 'true');
+  }
+
+  @Put(':id/read')
+  markAsRead(@Param('id') id: string) {
+    return this.notificationsService.markAsRead(id);
+  }
+}
