bed09bb4ef1ed70369898d0127e9bd6e913aae41
mkfinace <KIRANDAHIR@GMAIL.COM>
2026-09-03 20:51:25 +0530

    feat(portal): register customer portal module

---
+++ src/portal/portal.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+import { PortalController } from './portal.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+
+@Module({ imports: [PrismaModule], controllers: [PortalController] })
+export class PortalModule {}
